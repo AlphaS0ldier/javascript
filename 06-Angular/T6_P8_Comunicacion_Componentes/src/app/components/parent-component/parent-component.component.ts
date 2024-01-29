bc93b9fc2b5992821070ff55f31931de15f8de85
AlphaS0ldier <villaluzmichael19@gmail.com>
2024-01-29 17:59:15 +0100

    Modified parent

--- 06-Angular/T6_P8_Comunicacion_Componentes/src/app/components/parent-component/parent-component.component.ts
+++ 06-Angular/T6_P8_Comunicacion_Componentes/src/app/components/parent-component/parent-component.component.ts
@@ -1,10 +1,9 @@
-import { Component,Output} from '@angular/core';
+import { Component} from '@angular/core';
 @Component({
   selector: 'app-parent-component',
   templateUrl: './parent-component.component.html',
   styleUrl: './parent-component.component.css'
 })
 export class ParentComponentComponent {
-  @Output()
   public currentItem:string=" Hello World";
 }
